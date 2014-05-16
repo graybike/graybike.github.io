44a2aeeb5c8651020683faa0f46e9221e7a9431d
pavan <pavan@pavantrikutam.com>
2014-05-15 19:05:27 -0700

    Better taglines.

--- js/text-rotate.js
+++ js/text-rotate.js
@@ -1,37 +1,38 @@
-var verbs = ["design & build", "build", "craft", "create & revise"];
-var nouns = ["custom software", "websites", "mobile apps", "Shopify apps", "ecommerce stores", "user experiences", "content strategy"];
-var verbs = [];
-$(".rotate-nouns").text(nouns[0]);
-$(".rotate-verbs").text(verbs[0]);
-window.setInterval(function() {
-    var nounIndex = $.inArray($(".rotate-nouns").text(), nouns);
-    var verbIndex = $.inArray($(".rotate-verbs").text(), verbs);
-    nounIndex = nounIndex + 1;
-    if (nounIndex == nouns.length) nounIndex = 0;
-    console.log("NOUN INDEX: " + nounIndex);
-    $(".rotate-nouns").text(nouns[nounIndex]);
-    switch (nounIndex) {
-        case 0:
-        case 1:
-        case 2:
-            verbIndex = 0;
-            break;
-        case 3:
-        case 4:
-            verbIndex = 1;
-            break;
-        case 5:
-            verbIndex = 2;
-            break;
-        case 6:
-            verbIndex = 3;
-            break;
-        default:
-            break;
-    }
-    $(".rotate-verbs").text(verbs[verbIndex]);
-}, 2000);
-
+$(document).ready(function() {
+    var verbs = ["create","design & build","build", "craft", "revise"];
+    var nouns = ["custom software", "websites", "mobile apps", "Shopify stores & apps", "user experiences", "content strategy"];
+    $(".rotate-nouns").text(nouns[0]);
+    $(".rotate-verbs").text(verbs[0]);
+    window.setInterval(function() {
+        var nounIndex = $.inArray($(".rotate-nouns").text(), nouns);
+        var verbIndex = $.inArray($(".rotate-verbs").text(), verbs);
+        nounIndex = nounIndex + 1;
+        if (nounIndex == nouns.length) nounIndex = 0;
+        switch (nounIndex) {
+            case 0:
+                verbIndex = 0;
+                break;
+            case 1:
+            case 2:
+                verbIndex = 1;
+                break;
+            case 3:
+                verbIndex = 2;
+                break;
+            case 4:
+                verbIndex = 3;
+                break;
+            case 5:
+                verbIndex = 4;
+                break;
+            default:
+                break;
+        }
+        console.log(verbIndex);
+        $(".rotate-nouns").text(nouns[nounIndex]);
+        $(".rotate-verbs").text(verbs[verbIndex]);
+    }, 2000);
+});
 //   var i = 0;
 // $(".rotate-nouns").html("<span class='back'>" + nouns[0] + "</span>");
 // $(".rotate-verbs").html("<span class='back'>" + verbs[0] + "</span>");
@@ -64,7 +65,6 @@ window.setInterval(function() {
 //         rotateText($(".rotate-verbs"), verbIndex, verbs);
 //     }
 // }, 2000);
-
 // function rotateText(el, index, array) {
 //     if (el.find(".back").length > 0) {
 //         el.html(el.find(".back").html())
