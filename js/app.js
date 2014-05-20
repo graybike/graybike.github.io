77919d27cbc758c70c29c39e40bf600034fd1d97
pavan <pavan@pavantrikutam.com>
2014-05-19 19:01:22 -0700

    Added contact form functionality

--- js/app.js
+++ js/app.js
@@ -6,6 +6,33 @@ $(document).ready(function() {
   if ($(document).width() > 735) { //Ensure navigation links are at the right height if the screen is large enough.
     $(".nav-link").css("top", $(".nav-link").parent().height()/4);  
   }
+
+  $("#contact-form button[type='submit']").on("click", function(evt) {
+    evt.preventDefault();
+
+    if (!($("#form-name").val().length > 0 && $("#form-email").val().length > 0 && $("#form-project").val().length > 0)) {
+      $("#contact-form .sent").hide();
+      $("#contact-form .error").fadeIn();
+      return;
+    }
+
+    $.ajax({
+      dataType: 'jsonp',
+      url: "http://getsimpleform.com/messages/ajax?form_api_token=3983cc7d7269fc99bce6b5c0e0456326",
+      data: {
+        name: $("#form-name").val(),
+        email: $("#form-email").val(),
+        budget: $("#form-budget").val(),
+        project: $("#form-project").val(),
+      }
+    }).done(function() {
+      //callback which can be used to show a thank you message
+      //and reset the form
+      $("#contact-form .error").hide();
+      $("#contact-form .sent").fadeIn();
+
+    });
+  });
 });
 
 Foundation.utils.image_loaded($('.work img'), function(){
